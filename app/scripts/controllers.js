0bd7e7f147b3e67d072f87ad2952a9ee93b6421d
umairkhan14 <umairkhan14@outlook.com>
2017-10-01 03:54:15 +0500

    New contacts adding feature, to firebase, added

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,11 +1,42 @@
 angular.module('ContactsApp').controller('IndexController', ['$scope', '$firebaseArray', '$firebaseObject', function($scope, $firebaseArray, $firebaseObject){
 
+	$scope.profile = {
+		name: '',
+		email: '',
+		phone: ''
+	};
 
+	$scope.addForm = {
+		error: false,
+		success: false
+	};
 
 	var ref = firebase.database().ref();
-
 	$scope.contacts = $firebaseArray(ref);
-	$scope.object = $firebaseObject(ref);
+	//$scope.object = $firebaseObject(ref);
 
-	console.log($scope.object);
+
+	$scope.addContact = function() {
+		
+
+		$scope.contacts.$add({
+			name: $scope.profile.name,
+			email: $scope.profile.email,
+			phone: $scope.profile.phone
+		
+		}).then(function(ref){
+			console.log('profile added');
+			//var id = ref.key();
+			//console.log("Contact "+id+" added!");
+			$scope.addForm.success = true;
+
+			$scope.profile = {
+				name: '',
+				email: '',
+				phone: ''
+			};
+		});
+
+		return false;
+	}
 }]);
